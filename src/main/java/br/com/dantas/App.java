0a29e61918ed41897b5c1853739e7ed0905ad66a
Rodrigo Machado Dantas <rmdantas@users.noreply.github.com>
2021-02-20 20:11:27 -0200

    First commit

---
+++ src/main/java/br/com/dantas/App.java
@@ -0,0 +1,20 @@
+package br.com.dantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author rmdantas
+ *
+ * https://github.com/rmdantas/spring-security-authentication
+ */
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
